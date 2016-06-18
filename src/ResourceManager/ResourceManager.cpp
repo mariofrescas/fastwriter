75b3ef656cda9fc48cdf1007fefaaf23eb4915ba
Sysyfydev <sysyfydev@gmail.com>
2016-06-18 12:54:55 -0600

    Agrega el recurso fuente al proyecto

--- src/ResourceManager/ResourceManager.cpp
+++ src/ResourceManager/ResourceManager.cpp
@@ -5,7 +5,7 @@ ResourceManager::ResourceManager()
     textureHolder.load(Textures::ConcreteTexture1, "bg1.jpg");
     textureHolder.load(Textures::ConcreteTexture2, "bg2.jpg");
 
-    fontHolder.load(Fonts::Default, "saxmono.ttf");
+    fontHolder.load(Fonts::Default, "fonts/saxmono.ttf");
 }
 
 sf::Texture& ResourceManager::getTexture(const ResourceManager::Textures& textureId)
