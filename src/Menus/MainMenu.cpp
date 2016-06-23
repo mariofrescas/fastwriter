fc282230a782fad2c691da61a87339718bff5684
Sysyfydev <sysyfydev@gmail.com>
2016-06-23 08:30:09 -0600

    Agrega la escena del menu principal

--- src/Menus/MainMenu.cpp
+++ src/Menus/MainMenu.cpp
@@ -17,10 +17,15 @@
 
 #include "MainMenu.h"
 
+// WARNING: Uncomment to disable assertions
+// #define NDEBUG
+#include <cassert>
+
 #include "StateManager.h"
 #include "ResourceManager.h"
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 
 MainMenu::MainMenu(StateManager& stateManager)
     : State(stateManager)
@@ -28,14 +33,26 @@ MainMenu::MainMenu(StateManager& stateManager)
     using Textures = ResourceManager::Textures;
     ResourceManager& resMngr = getStateManager().getSharedContext().resourceManager;
 
+    initMenuRects();
+    initMenuSprites();
+    initMenuPositions();
+
     background.setTexture(resMngr.getTexture(Textures::Background));
 }
 
 void MainMenu::handleInput(const sf::Event& event)
 {
+    if (event.type == sf::Event::MouseMoved)
+    {
+        handleMouseMoved(event.mouseMove.x, event.mouseMove.y);
+    }
+    else if (event.type == sf::Event::MouseButtonPressed)
+    {
+        handleMousePressed(event.mouseButton.x, event.mouseButton.y);
+    }
 }
 
-void MainMenu::update(const sf::Time& dt)
+void MainMenu::update(const sf::Time&)
 {
 }
 
@@ -44,4 +61,119 @@ void MainMenu::draw()
     sf::RenderWindow& window = getStateManager().getSharedContext().window;
 
     window.draw(background);
+
+    for (const auto& opt : menuSprites)
+    {
+        window.draw(opt.second);
+    }
+}
+
+void MainMenu::handleMouseMoved(int x, int y)
+{
+    bool found = false;
+    for (const auto& opt : menuSprites)
+    {
+        if (opt.second.getGlobalBounds().contains(x, y))
+        {
+            setCurrentOption(opt.first);
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+    {
+        setCurrentOption(Options::None);
+    }
+}
+
+void MainMenu::setCurrentOption(const Options& option)
+{
+    if (currentOption != option)
+    {
+        if (option == Options::None)
+        {
+            menuSprites[currentOption].setTextureRect(menuRects[currentOption].first);
+        }
+        else
+        {
+            if (currentOption != Options::None)
+            {
+                menuSprites[currentOption].setTextureRect(menuRects[currentOption].first);
+            }
+
+            menuSprites[option].setTextureRect(menuRects[option].second);
+        }
+
+        currentOption = option;
+    }
+}
+
+void MainMenu::handleMousePressed(int x, int y)
+{
+    for (const auto& opt : menuSprites)
+    {
+        if (opt.second.getGlobalBounds().contains(x, y))
+        {
+            menuOptionPressed(opt.first);
+            break;
+        }
+    }
+}
+
+void MainMenu::menuOptionPressed(const Options& option)
+{
+    switch (option)
+    {
+    case Options::NewGame:
+        break;
+    case Options::Scores:
+        break;
+    case Options::Help:
+        break;
+    case Options::About:
+        break;
+    case Options::Exit:
+        getStateManager().getSharedContext().window.close();
+        break;
+    default:
+        assert(false);
+        break;
+    }
+}
+
+void MainMenu::initMenuRects()
+{
+    menuRects[Options::NewGame] = std::make_pair(sf::IntRect(0, 0, 661, 146), sf::IntRect(662, 0, 661, 146));
+    menuRects[Options::Scores] = std::make_pair(sf::IntRect(0, 147, 661, 146), sf::IntRect(662, 147, 661, 146));
+    menuRects[Options::Help] = std::make_pair(sf::IntRect(0, 294, 661, 146), sf::IntRect(662, 294, 661, 146));
+    menuRects[Options::About] = std::make_pair(sf::IntRect(0, 441, 661, 146), sf::IntRect(662, 441, 661, 146));
+    menuRects[Options::Exit] = std::make_pair(sf::IntRect(0, 588, 661, 146), sf::IntRect(662, 588, 661, 146));
+}
+
+void MainMenu::initMenuSprites()
+{
+    using Textures = ResourceManager::Textures;
+    ResourceManager& resMngr = getStateManager().getSharedContext().resourceManager;
+
+    menuSprites[Options::NewGame] = sf::Sprite(resMngr.getTexture(Textures::MainMenu));
+    menuSprites[Options::Scores] = sf::Sprite(resMngr.getTexture(Textures::MainMenu));
+    menuSprites[Options::Help] = sf::Sprite(resMngr.getTexture(Textures::MainMenu));
+    menuSprites[Options::About] = sf::Sprite(resMngr.getTexture(Textures::MainMenu));
+    menuSprites[Options::Exit] = sf::Sprite(resMngr.getTexture(Textures::MainMenu));
+
+    for (auto& opt : menuSprites)
+    {
+        opt.second.setTextureRect(menuRects[opt.first].first);
+    }
+}
+
+void MainMenu::initMenuPositions()
+{
+    const int width = getStateManager().getSharedContext().window.getSize().x;
+
+    menuSprites[Options::NewGame].setPosition((width / 2) - (menuSprites[Options::NewGame].getTextureRect().width / 2), 17);
+    menuSprites[Options::Scores].setPosition((width / 2) - (menuSprites[Options::Scores].getTextureRect().width / 2), 164);
+    menuSprites[Options::Help].setPosition((width / 2) - (menuSprites[Options::Help].getTextureRect().width / 2), 311);
+    menuSprites[Options::About].setPosition((width / 2) - (menuSprites[Options::About].getTextureRect().width / 2), 458);
+    menuSprites[Options::Exit].setPosition((width / 2) - (menuSprites[Options::Exit].getTextureRect().width / 2), 605);
 }
--- src/Menus/MainMenu.h
+++ src/Menus/MainMenu.h
@@ -24,19 +24,150 @@
 /// \brief Inclusiones internas
 ///
 ////////////////////////////////////////////////////////////
+#include <map>
 #include <SFML/Graphics/Sprite.hpp>
 
+////////////////////////////////////////////////////////////
+/// \brief Describe la escena del menu principal
+///
+////////////////////////////////////////////////////////////
 class MainMenu : public State
 {
+private:
+    ////////////////////////////////////////////////////////////
+    /// \brief Describe las opciones que tiene el menu
+    ///
+    ////////////////////////////////////////////////////////////
+    enum class Options
+    {
+        None,    ///< Ninguna opcion
+        NewGame, ///< Juego nuevo
+        Scores,  ///< Puntuaciones
+        Help,    ///< Ayuda
+        About,   ///< Informacion
+        Exit     ///< Salir del juego
+    };
+
+    using RectPair = std::pair<sf::IntRect, sf::IntRect>;
+
 public:
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Inicializa la escena del menu principal
+    ///
+    /// Inicializa:
+    /// * Sprite del fondo
+    /// * Sprites del menu
+    /// * Posicion de los sprites en la textura
+    /// * Posicion de los sprites en la escena
+    ///
+    ////////////////////////////////////////////////////////////
     MainMenu(StateManager& stateManager);
 
+    ////////////////////////////////////////////////////////////
+    /// \brief Actualiza la logica de la escena del menu principal
+    /// \param event Evento producido por el usuario
+    /// \see handleMouseMoved
+    /// \see handleMousePressed
+    ///
+    /// Llama a handelMouseMove en caso de que el cursor se mueva
+    /// o llama a handleMousePressed en caso de que se de un click.
+    ///
+    ////////////////////////////////////////////////////////////
     virtual void handleInput(const sf::Event& event) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief No hace nada
+    ///
+    /// No hace nada debido a que no es necesario actualizar los
+    /// elementos graficos de la escena en base a la logica.
+    ///
+    ////////////////////////////////////////////////////////////
     virtual void update(const sf::Time& dt) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Dibuja la escena del menu principal
+    ///
+    /// Dibuja:
+    /// * Sprite del fondo
+    /// * Sprites del menu
+    ///
+    ////////////////////////////////////////////////////////////
     virtual void draw() override;
 
 private:
-    sf::Sprite background;
+    sf::Sprite                    background;  ///< Sprite del fondo
+    std::map<Options, RectPair>   menuRects;   ///< Posiciones de los sprites en la textura
+    std::map<Options, sf::Sprite> menuSprites; ///< Sprites del menu
+    Options currentOption = Options::None;     ///< Opcion seleccionada
+
+private:
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Identfica cual es la opcion del menu en la que se encuentra el cursor
+    /// \param x Coordenada en el eje X
+    /// \param y Coordenada en el eje Y
+    /// \see setCurrentOption
+    ///
+    /// Indica a setCurrentOption que cambie la opcion seleccionada.
+    ///
+    ////////////////////////////////////////////////////////////
+    void handleMouseMoved(int x, int y);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Cambia la posicion del sprite en la textura de la opcion seleccionada
+    /// \param option Opcion del menu en la que se encuentra el cursor
+    /// \see handleMouseMoved
+    ///
+    /// Cambia la posicion del sprite de la opcion proporcionada
+    /// dentro de la textura para que tenga una imagen de seleccionada.
+    ///
+    ////////////////////////////////////////////////////////////
+    void setCurrentOption(const Options& option);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Identfica cual es la opcion del menu a la que se dio click
+    /// \param x Coordenada en el eje X
+    /// \param y Coordenada en el eje Y
+    /// \see menuOptionPressed
+    ///
+    /// Indica a menuOptionPressed que reaccione a la opcion identificada.
+    ///
+    ////////////////////////////////////////////////////////////
+    void handleMousePressed(int x, int y);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Reacciona al click del usuario en alguna opcion del menu
+    /// \param option Opcion del menu en la que se dio click
+    /// \see handleMousePressed
+    ///
+    /// Acciones:
+    /// * NewGame:
+    /// * Scores:
+    /// * Help:
+    /// * About:
+    /// * Exit: Termina la aplicacion
+    ///
+    ////////////////////////////////////////////////////////////
+    void menuOptionPressed(const Options& option);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Inicializa las posiciones de los sprites en la textura
+    ///
+    ////////////////////////////////////////////////////////////
+    void initMenuRects();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Inicializa los sprites del menu
+    ///
+    ////////////////////////////////////////////////////////////
+    void initMenuSprites();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Inicializa las posiciones de los sprites del menu
+    ///
+    ////////////////////////////////////////////////////////////
+    void initMenuPositions();
 };
 
 #endif // MAINMENU_H
